1656c4863195b831bf2dba926c88eb16874edac6
Yasser Rabhi <yesser2010@gmail.com>
2021-10-28 19:27:41 +0100

    Update tbl_users.sql

--- tbl_users.sql
+++ tbl_users.sql
@@ -1,1 +1,1 @@
-CREATE TABLE `db_connect`.`tbl_use` ( `Id` INT(11) NOT NULL AUTO_INCREMENT , `Name` INT(50) NOT NULL , `Email` INT(150) NOT NULL , `Password` INT(100) NOT NULL , PRIMARY KEY (`Id`)) ENGINE = InnoDB;
+CREATE TABLE `db_connect`.`tbl_users` ( `Id` INT(11) NOT NULL AUTO_INCREMENT , `Name` INT(50) NOT NULL , `Email` INT(150) NOT NULL , `Password` INT(100) NOT NULL , PRIMARY KEY (`Id`)) ENGINE = InnoDB;
